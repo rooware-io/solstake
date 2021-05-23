517e2be74e9e5e8742802a3159c919c987992452
Arrowana <8245419+Arrowana@users.noreply.github.com>
2021-05-23 18:43:09 +1000

    Use HashRouter to avoid 404

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Route
 } from 'react-router-dom';
 import DApp from './views/DApp';
