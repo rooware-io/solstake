151b4b1986064dcd4b8391bfa7142524b1dcf187
Arrowana <8245419+Arrowana@users.noreply.github.com>
2021-06-08 18:16:46 +1000

    fix whitespace

--- src/contexts/epoch.tsx
+++ src/contexts/epoch.tsx
@@ -16,42 +16,42 @@ export const EpochContext = React.createContext<EpochConfig>({
 });
 
 export function EpochProvider({ children = undefined as any }) {
-    const connection = useConnection();
-    const [epochInfo, setEpochInfo] = useState<EpochInfo>();
-    const [epochSchedule, setEpochSchedule] = useState<EpochSchedule>();
-    const [epochStartTime, setEpochStartTime] = useState<number>();
+  const connection = useConnection();
+  const [epochInfo, setEpochInfo] = useState<EpochInfo>();
+  const [epochSchedule, setEpochSchedule] = useState<EpochSchedule>();
+  const [epochStartTime, setEpochStartTime] = useState<number>();
 
-    useEffect(() => {
-        setEpochInfo(undefined);
-        connection.getEpochInfo().then(setEpochInfo);
-    }, [connection]);
+  useEffect(() => {
+      setEpochInfo(undefined);
+      connection.getEpochInfo().then(setEpochInfo);
+  }, [connection]);
 
-    useEffect(() => {
-      setEpochSchedule(undefined);
-      connection.getEpochSchedule()
-        .then(setEpochSchedule);
-    }, [connection]);
+  useEffect(() => {
+    setEpochSchedule(undefined);
+    connection.getEpochSchedule()
+      .then(setEpochSchedule);
+  }, [connection]);
 
-    useEffect(() => {
-      if(!epochInfo || !epochSchedule) {
-        setEpochStartTime(undefined);
-        return;
-      }
+  useEffect(() => {
+    if(!epochInfo || !epochSchedule) {
+      setEpochStartTime(undefined);
+      return;
+    }
 
-      const slot = getFirstSlotInEpoch(epochSchedule, epochInfo.epoch);
-      getFirstBlockTime(connection, slot)
-        .then(setEpochStartTime);
-    }, [connection, epochInfo]);
+    const slot = getFirstSlotInEpoch(epochSchedule, epochInfo.epoch);
+    getFirstBlockTime(connection, slot)
+      .then(setEpochStartTime);
+  }, [connection, epochInfo]);
 
-    return (
-        <EpochContext.Provider
-          value={{
-            epochInfo,
-            epochSchedule,
-            epochStartTime
-          }}
-        >
-          {children}
-        </EpochContext.Provider>
-      );
+  return (
+    <EpochContext.Provider
+      value={{
+        epochInfo,
+        epochSchedule,
+        epochStartTime
+      }}
+    >
+      {children}
+    </EpochContext.Provider>
+  );
 }
