2ff04a3f1d218e8d30469d0806e0cf5b33585626
Arrowana <8245419+Arrowana@users.noreply.github.com>
2022-01-31 11:01:58 +1100

    Can run without exploding

--- src/contexts/wallet.tsx
+++ src/contexts/wallet.tsx
@@ -16,7 +16,7 @@ import { useSnackbar } from 'notistack';
 
 export const Wallet: FC<{ children: ReactNode }> = ({ children }) => {
   // The network can be set to 'devnet', 'testnet', or 'mainnet-beta'.
-  const network = WalletAdapterNetwork.Devnet;
+  const network = WalletAdapterNetwork.Mainnet;
 
   // You can also provide a custom RPC endpoint.
   const endpoint = useMemo(() => clusterApiUrl(network), [network]);
